bb745c6495990b8828e80c5123952855e90adf25
Antonin Delpeuch <antonin@delpeuch.eu>
2012-08-28 22:01:07 +0200

    Working PDF viewer, introduction of Popcorn JS.

--- pdftopng.sh
+++ pdftopng.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+rm -f $1-big-*.png
 convert -density 200 -quality 100 -resize x600 $1.pdf $1-big.png
-convert -resize x200 $1.pdf $1-small.png
+rm -f $1-small-*.png
+convert -resize x100 $1.pdf $1-small.png
 
