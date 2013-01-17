6edcbd797d759d093eb3f5e2769bc516ee4d7780
Antonin Delpeuch <antonin@delpeuch.eu>
2013-01-17 23:57:45 +0100

    Fix time update

--- stream.sh
+++ stream.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-avconv -f video4linux2 -i /dev/video0 -c:a pcm_u8 -c:v libtheora -f ogg - | oggfwd localhost 8000 hackme live.ogg
+avconv -f video4linux2 -i /dev/video0 -c:a pcm_u8 -c:v libtheora -f ogg - | oggfwd vks 8000 hackme live.ogg
 
 
