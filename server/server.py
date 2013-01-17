454b64422af6e8732c799e314512b45d7f91f2b8
Antonin Delpeuch <antonin@delpeuch.eu>
2013-01-17 09:35:53 +0100

    Added conference management features

--- server/server.py
+++ server/server.py
@@ -2,15 +2,17 @@ import tornado.ioloop
 import tornado.web
 import sys
 
-class SlideContext:
+live_video_URI = "http://localhost:8000/live.ogg"
 
+class SlideContext:
     def __init__(self):
         self.slideIdx = 0 
+        self.running = False
         self.confId = "keller-comptheory"
         self.nbSlides = 5
-        self.title = "Live @ fabelier (updated)"
-        self.author = "team"
-        self.videoURI = "http://localhost:8000/live.ogg"
+        self.title = "Default conf"
+        self.author = "Default author"
+        self.videoURI = live_video_URI 
         self.waitingCallbacks = []
 
     def currentSlide(self):
@@ -50,11 +52,16 @@ class LiveHandler(tornado.web.RequestHandler):
                      "\", \"title\": \""+ctxt.title+
                      "\", \"author\": \""+ctxt.author+
                      "\", \"videoURI\": \""+ctxt.videoURI+
+                     "\", \"running\": \""+str(ctxt.running)+
                      "\" }")
             self.finish()
         elif command == "incr":
             self.db["context"].incrSlide()
             self.write("Incremented.\nNew value is "+str(self.db["context"].currentSlide()))
+            self.finish()
+        elif command == "set":
+            self.db["context"].slideIdx = self.get_argument("i",self.db["context"].slideIdx)
+            self.write(str(self.db["context"].currentSlide()))
             self.finish()
         elif command == "waitNext":
             self.db["context"].registerWaiter(self.slideChanged)
@@ -65,6 +72,16 @@ class LiveHandler(tornado.web.RequestHandler):
         else:
             self.write("Unknown command")
             self.finish()
+
+    def post(self, command):
+        if command == "new":
+            ctxt.confId = self.get_argument("conf_id", "new_conf")
+            ctxt.currentSlide = 0
+            ctxt.nbSlides = self.get_argument("nb_slides", 1)
+            ctxt.title = self.get_argument("title", "New conference")
+            ctxt.author = self.get_argument("author", "The author")
+            ctxt.videoURI = live_video_URI
+            ctxt.running = True
     
     def unregisterWaiter(self):
         if self.registered:
